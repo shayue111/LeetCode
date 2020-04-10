392e79359d6077e4f2e845cf087fe84ad90dc36e
wangt <wt0504@zju.edu.cn>
2020-04-10 22:49:19 +0800

    Question151

---
+++ src/Question151.scala
@@ -0,0 +1,14 @@
+import scala.collection.mutable.ArrayBuffer
+
+object Question151 {
+    def main(args: Array[String]): Unit = {
+        println(reverseWords("the sky is blue"))
+        println(reverseWords(" hello world!"))
+        println(reverseWords("a good  example"))
+    }
+
+    def reverseWords(s: String): String = {
+        val arr = s.split(" ").toBuffer
+        arr.filter(_ != "").reverse.mkString(" ")
+    }
+}
